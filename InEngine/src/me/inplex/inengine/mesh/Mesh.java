c4ff026a7f8294e4ec080d21cd0cc38ba1d80e0f
inplex <christl.joshua@googlemail.com>
2014-04-26 21:26:31 +0200

    fixes

--- InEngine/src/me/inplex/inengine/mesh/Mesh.java
+++ InEngine/src/me/inplex/inengine/mesh/Mesh.java
@@ -1,6 +1,11 @@
 package me.inplex.inengine.mesh;
 
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.glBegin;
 import static org.lwjgl.opengl.GL11.glColor3f;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glNormal3f;
+import static org.lwjgl.opengl.GL11.glVertex3f;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,7 +47,7 @@ public class Mesh {
 		glColor3f(0f, 0f, 0f);
 		for (Face face : faces) {
 			glColor3f(0f, 0f, 0f);
-			GL11.glBegin(face.isEdge() ? GL11.GL_TRIANGLES : GL11.GL_QUADS);
+			GL11.glBegin(GL11.GL_LINES);
 			for (int i = 0; i < (face.isEdge() ? 2 : 3); i++) {
 				Vector3f vertex = face.getVertices()[i];
 				GL11.glVertex3f(vertex.x, vertex.y, vertex.z);
@@ -54,6 +59,32 @@ public class Mesh {
 			GL11.glEnd();
 		}
 		glColor3f(1f, 1f, 1f);
+	}
+	
+	public void render2() {
+		glColor3f(1f, 1f, 1f);
+		glBegin(GL_TRIANGLES);
+		for (Face face : faces) {
+			if(face.hasNormals()) {
+				Vector3f n1 = face.normals[0];
+				glNormal3f(n1.x, n1.y, n1.z);
+			}
+			Vector3f v1 = face.vertices[0];
+			glVertex3f(v1.x,v1.y,v1.z);
+			if(face.hasNormals()) {
+				Vector3f n2 = face.normals[1];
+				glNormal3f(n2.x, n2.y, n2.z);
+			}
+			Vector3f v2 = face.vertices[1];
+			glVertex3f(v2.x,v2.y,v2.z);
+			if(face.hasNormals()) {
+				Vector3f n3 = face.normals[2];
+				glNormal3f(n3.x, n3.y, n3.z);
+			}
+			Vector3f v3 = face.vertices[2];
+			glVertex3f(v3.x,v3.y,v3.z);
+		}
+		glEnd();
 	}
 
 	public List<Face> getFaces() {
--- InEngine/src/me/inplex/inengine/test/TestGame.java
+++ InEngine/src/me/inplex/inengine/test/TestGame.java
@@ -11,11 +11,13 @@ public class TestGame extends Game {
 	@Override
 	protected void onInit() {
 		Entity entity1 = new Entity(ObjLoader.load("res//test1.obj"), 0f, 1f, 0f);
-		Entity entity2 = new Entity(ObjLoader.load("res//test2.obj"), 10f, 1f, 0f);
-		Entity entity3 = new Entity(ObjLoader.load("res//test3.obj"), 20f, 1f, 0f);
+		Entity entity2 = new Entity(ObjLoader.load("res//test2.obj"), 5f, 1f, 0f);
+		Entity entity3 = new Entity(ObjLoader.load("res//test3.obj"), 10f, 1f, 0f);
+		Entity entity4 = new Entity(ObjLoader.load("res//test4.obj"), 15f, 1f, 0f);
 		getRenderer().getWorld().add(entity1);
 		getRenderer().getWorld().add(entity2);
 		getRenderer().getWorld().add(entity3);
+		getRenderer().getWorld().add(entity4);
 	}
 
 	
