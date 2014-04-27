718db464c926f416297ebe16c7fc4c9b4deed778
matthinc <wurstbreze@gmail.com>
2014-04-27 19:36:51 +0200

    Display Lists

--- InEngine/src/me/inplex/inengine/mesh/Mesh.java
+++ InEngine/src/me/inplex/inengine/mesh/Mesh.java
@@ -2,34 +2,55 @@ package me.inplex.inengine.mesh;
 
 import static org.lwjgl.opengl.GL11.glColor3f;
 
+import java.nio.IntBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.vector.Vector3f;
+import static org.lwjgl.opengl.ARBBufferObject.*;
+import static org.lwjgl.opengl.ARBVertexBufferObject.*;
+
 
 public class Mesh {
+	
+	private enum RenderMethod{
+		NORMAL,
+		VBO,
+		LIST;
+	}
+	
+	final RenderMethod renderMethod = RenderMethod.LIST;
 
 	private static int lastId = 0;
 	
 	private List<Vector3f> vertices;
 	private List<Vector3f> normals;
 	private List<Face> faces;
-
 	private int id;
+	private int displayList;
 
 	public Mesh() {
 		this.vertices = new ArrayList<Vector3f>();
 		this.normals = new ArrayList<Vector3f>();
 		this.faces = new ArrayList<Face>();
 		this.id = lastId++;
+		
+		if (renderMethod==RenderMethod.LIST){
+			this.displayList = GL11.glGenLists(1);
+			fillDisplayList();
+		}
+		
 	}
-
-	// TODO: Make this better (use VBO, VertexAttribArray)
-	public void render() {
+	
+	
+	private void fillDisplayList(){
+		GL11.glNewList(displayList, GL11.GL_COMPILE);
+		
 		glColor3f(1f, 1f, 1f);
+		GL11.glBegin(GL11.GL_TRIANGLES);
 		for (Face face : faces) {
-			GL11.glBegin(GL11.GL_TRIANGLES);
 			for (int i = 0; i < (face.isEdge() ? 2 : 3); i++) {
 				Vector3f vertex = vertices.get(face.getVertexIds()[i]-1);
 				GL11.glVertex3f(vertex.x, vertex.y, vertex.z);
@@ -38,9 +59,8 @@ public class Mesh {
 					GL11.glNormal3f(normal.x, normal.y, normal.z);
 				}
 			}
-			GL11.glEnd();
 		}
-		
+		GL11.glEnd();
 		// Draw Outline
 		glColor3f(0f, 0f, 0f);
 		for (Face face : faces) {
@@ -54,6 +74,47 @@ public class Mesh {
 				}
 			}
 			GL11.glEnd();
+		}
+		
+		GL11.glEndList();
+	}
+
+
+	public void render() {
+		if (renderMethod==RenderMethod.NORMAL){	//Render Normal (GL11)
+		glColor3f(1f, 1f, 1f);
+		GL11.glBegin(GL11.GL_TRIANGLES);
+		for (Face face : faces) {
+			for (int i = 0; i < (face.isEdge() ? 2 : 3); i++) {
+				Vector3f vertex = vertices.get(face.getVertexIds()[i]-1);
+				GL11.glVertex3f(vertex.x, vertex.y, vertex.z);
+				if (face.hasNormals() && i < 3) {
+					Vector3f normal = normals.get(face.getNormalIds()[i]-1);
+					GL11.glNormal3f(normal.x, normal.y, normal.z);
+				}
+			}
+		}
+		GL11.glEnd();
+		// Draw Outline
+		glColor3f(0f, 0f, 0f);
+		for (Face face : faces) {
+			GL11.glBegin(GL11.GL_LINES);
+			for (int i = 0; i < (face.isEdge() ? 2 : 3); i++) {
+				Vector3f vertex = vertices.get(face.getVertexIds()[i]-1);
+				GL11.glVertex3f(vertex.x, vertex.y, vertex.z);
+				if (face.hasNormals() && i < 3) {
+					Vector3f normal = normals.get(face.getNormalIds()[i]-1);
+					GL11.glNormal3f(normal.x, normal.y, normal.z);
+				}
+			}
+			GL11.glEnd();
+		}
+
+		
+		}else if(renderMethod==RenderMethod.VBO){ //Render using VBOs
+			//TODO VBO rendering
+		}else if(renderMethod==RenderMethod.LIST){ //Render using lists
+			GL11.glCallList(displayList);
 		}
 	}
 
